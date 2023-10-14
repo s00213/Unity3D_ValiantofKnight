b20d02b7a92a3b935222bf85f55f8e2c5fe0dcac
KANG SOO IN <ks00in213@gmail.com>
2023-10-14 15:58:18 +0900

    fix : 수정

--- Assets/Scripts/Interface/IListenable.cs
+++ Assets/Scripts/Interface/IListenable.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Enemy
+/// </summary>
 public interface IListenable
 {
 	public void Listen(Transform trans);
