c9b3424b7a4f018aa033e0098da9a3809b8677d2
KANG SOO IN <ks00in213@gmail.com>
2023-10-20 02:54:53 +0900

    feat : MovableHeaderUI 스크립트 추가

--- Assets/Scripts/UI/Inventory UI/MovableHeaderUI.cs
+++ Assets/Scripts/UI/Inventory UI/MovableHeaderUI.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MovableHeaderUI : MonoBehaviour
+public class MovableHeaderUI : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private Transform tartgetTransform; // UI
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector2 startPoint;
+	private Vector2 movePoint;
+
+	private void Awake()
+	{
+		// 이동 대상 UI를 지정하지 않은 경우, 자동으로 부모로 초기화
+		if (tartgetTransform == null)
+		tartgetTransform = transform.parent;
+	}
+
+	// 드래그 : 마우스 커서 위치로 이동
+	void IDragHandler.OnDrag(PointerEventData eventData)
+	{
+		tartgetTransform.position = startPoint + (eventData.position - movePoint);
+	}
+
+	// 드래그 시작 위치 지정
+	void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+	{
+		startPoint = tartgetTransform.position;
+		movePoint = eventData.position;
+	}
 }
