edf7b1d8bdc67691dcfd126ef172b3abffde0b53
KANG SOO IN <ks00in213@gmail.com>
2023-10-20 02:57:54 +0900

    fix : MovableUI로 class 클래스 이름 수정

--- Assets/Scripts/UI/Inventory UI/MovableHeaderUI.cs
+++ Assets/Scripts/UI/Inventory UI/MovableHeaderUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MovableHeaderUI : MonoBehaviour, IDragHandler, IPointerDownHandler
+public class MovableUI : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
 	[SerializeField] private Transform tartgetTransform; // UI
 
